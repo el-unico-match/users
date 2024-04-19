46acfae787f9577dfb97abc5a6f979a15e0878e7
rafaelputaro <rafaelputaro@gmail.com>
2024-04-19 12:21:44 -0300

    agrego impresión de ruta db

--- database/config.js
+++ database/config.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 
 const dbConnection = async () => {
     try {
-        await mongoose.connect(process.env.DB_CNN, {
+        let db_cnn = process.env.DB_CNN;
+        await mongoose.connect(db_cnn, {
            // useNewUrlParser: true,
             //useUnifiedTopology: true,
         });
-        console.log('Base de datos de usuarios Online');
+        console.log('Base de datos de usuarios Online: ', db_cnn);
     } catch (error) {
         console.log(error);
         throw new Error('Error al iniciar la base de datos de usuarios');
