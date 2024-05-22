ba12276035deaa251732315fe4299f927e8600d0
Nicolas <58600338+NicolasZanardo@users.noreply.github.com>
2024-05-21 22:47:31 -0300

    Modificar accion y confuguracion de jest para intentar publicar resultados de coveral

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports =  {
     testTimeout: 70000, 
-    testEnvironment: 'node'
+    testEnvironment: 'node',
+    collectCoverage: true,
+    coverageDirectory: 'coverage',
+    coverageReporters: ['lcov', 'text']
 };
