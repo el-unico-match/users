c32d77792a20da2c6d21768e0d0c01ed57c18bb4
Martin Hernan Gomez <mhgomez@fi.uba.ar>
2024-05-27 17:03:56 -0300

    Moves /current endpoint into user router's scope, as it is inside the /user path.

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@
 */
 
 const {Router} = require('express');
+const {getDataUser} = require('../controllers/current');
 const {
     validateJWT,
     validateLazyJWT} = require('../middlewares/validateJWT');
@@ -97,6 +98,88 @@ const router = Router();
  *                                  example: Please talk to the administrator
 */
 router.post('/',validateLazyJWT, checkCreateUser, createUser);
+
+/**
+ * @swagger
+ * /api/user/current:
+ *  get:
+ *      summary: get data from the user associated with the token 
+ *      tags: [User]
+ *      parameters:
+ *          - in: header
+ *            name: x-token
+ *            schema:
+ *              type: string
+ *              required: true
+ *              description: user token
+ *      responses:
+ *          200: 
+ *              description: return user data!
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              ok:
+ *                                  type: boolean
+ *                                  example: true
+ *                              user:
+ *                                  type: object
+ *                                  $ref: '#/components/schemas/UserSharedData'
+ *          400:
+ *              description: return error no token!
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              ok:
+ *                                  type: boolean
+ *                                  example: false
+ *                              msg:
+ *                                  type: string
+ *                                  example: There is no token in the request
+ *          401:
+ *              description: return error invalid token!
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              ok:
+ *                                  type: boolean
+ *                                  example: false
+ *                              msg:
+ *                                  type: string
+ *                                  example: Invalid token
+ *          404:
+ *              description: return error not found!
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              ok:
+ *                                  type: boolean
+ *                                  example: false
+ *                              msg:
+ *                                  type: string
+ *                                  example: The user does not exist
+ *          500:
+ *              description: return internal error!
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              ok:
+ *                                  type: boolean
+ *                                  example: false
+ *                              msg:
+ *                                  type: string
+ *                                  example: Please talk to the administrator
+*/
+router.get('/current', validateJWT, getDataUser);
 
 /**
  * @swagger
